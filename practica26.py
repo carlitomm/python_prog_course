3b321a555f09685fc6675e636e31e4e18d0d02bf
carlitomm <cmiguelezmachado@gmail.com>
2021-04-06 01:16:24 -0400

    practica26

--- practica26.py
+++ practica26.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import serial
+import collections
 
-def getSerialData(serialConnection):
-    value = serialConnection.readline().rstrip()
-    return value
+def getSerialData (self, Samples, serialConnection, lines, lineValueText, lineLabel):
+    value = float(serialConnection.readline().rstrip())
+    if value > 0:
+        ax.set_facecolor('r')
+    else: 
+        ax.set_facecolor('g')
+    data.append(value)
+    lines.set_data(range(Samples),data)
+    lineValueText.set_text(lineLabel + " = " + str(round(value,2)))
 
 serialPort = '/dev/ttyUSB0'
 baudRate = 9600
@@ -17,17 +24,33 @@ except Exception as e:
     print("error con el puerto serie")
     print(e)
 
+Samples = 100
+data = collections.deque([0]*Samples, maxlen=Samples)
+sampleTime = 100
+
 xmin = 0
-xmax = 100
+xmax = Samples
 ymin = -1
 ymax = 1
 
 fig = plt.figure(figsize=(13,6))
 ax = plt.axes(xlim=(xmin,xmax), ylim=(ymin, ymax))
-ax.set_xlabel("muestras")
-ax.set_ylabel("muestras")
+ax.set_xlabel("Tiempo")
+ax.set_ylabel("Ritmo Cardiaco")
 
-lines = ax.plot([],[], label="hello")
-while (True):
-    print(getSerialData(serialConnection))
+lineLabel = "voltaje"
+lines = ax.plot([],[], label=lineLabel)[0]
+lineValueText = ax.text(0.85, 0.95,'', transform=ax.transAxes)
+
+
+anim = animation.FuncAnimation(
+    fig, 
+    getSerialData, 
+    fargs=(Samples, serialConnection, lines, lineValueText, lineLabel),
+    interval = sampleTime
+    )
+plt.show()
+
+serialConnection.close()
+
 
--- practica26/practica26.ino
+++ practica26/practica26.ino
@@ -1,17 +1,18 @@
+const int potInt = A2;
 
 float x = 0;
 float value;
 void setup() {
   Serial.begin(9600);
-  
 }
 
 void loop() {
-  
+  value = analogRead(A2);
   for (x=-3.14;x<3.14;x+=0.1){
     value = sin(x);
+    value = value + 1;
+    value = value * 40;
     Serial.println(value);
   }
   delay(100);
-  
 }
